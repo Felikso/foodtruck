3ca3bd2816a7fee29f9d75862cfd24ec165cf9a6
Felikso <aleksandrasniegucka@wp.pl>
2021-05-11 02:45:52 +0200

    edited opening hours

--- src/components/OpeningHoursDropDown/DropDown.js
+++ src/components/OpeningHoursDropDown/DropDown.js
@@ -25,8 +25,40 @@ export default class Dropdown extends React.Component {
 
   const plDay = weekDaysPl[todayDay]
 
+  const todayObj = openingData.filter(item => item.day == plDay)
+
+
+
+  const toCloseHours = (todayObj[0].close - todayHour) > 0 ? todayHour - todayObj[0].close : todayObj[0].close - todayHour
+  const toCloseMinutes = 0 - todayMinutes
+
+
+
+  console.log(todayObj[0].close)
+
+  let closeMin = 
+  todayMinutes>0 ? 
+  `${60 - todayMinutes} min.` :
+  ""
+
+  let closeHours = 
+  toCloseHours>0 ? 
+  `${toCloseHours} godz.` :
+  ""
+
+  let timeToClose = closeHours +" "+ closeMin
+
+  let closeInfo = toCloseHours>0 ? timeToClose : "zamknięte"
+
+
+
+  console.log(closeInfo)
+
+  console.log(timeToClose)
+  console.log('timeToClose')
+  console.log(toCloseHours)
     let className = "dropdown-slidedown";
-    let caption = this.props.open ? "zwiń listę" : `dziś ${plDay} godzina: ${todayHour}:${todayMinutes < 10 ? '0'+todayMinutes : todayMinutes}`;
+    let caption = this.props.open ? closeInfo : `dziś ${plDay} godzina: ${todayHour}:${todayMinutes < 10 ? '0'+todayMinutes : todayMinutes}`;
     let render = this.props.open;
     let closed = false;
 
@@ -61,16 +93,77 @@ export default class Dropdown extends React.Component {
   const date = new Date();
   const todayHour = date.getHours()
   const todayDay = date.getDay()
+  const todayMinutes = date.getMinutes()
 
   const plDay = weekDaysPl[todayDay]
   
       items.push(
-        openingData.map((item, idx) => (
+        openingData.map((item, idx) => {
+
+          console.log("XDDDD")
+
+          const hoursCloseList = item.open - todayHour
+
+          const hoursOpenList = item.close - todayHour
+
+          const hoursOpenListPrev = item.prevClose - todayHour
+
+          const closeMin = 
+          todayMinutes>0 ? 
+          `${60 - todayMinutes} min.` :
+          ""
+        
+          const closeHoursMore = 
+          hoursCloseList>0 ?
+          `${hoursCloseList-1} godz.` :
+          ""
+
+          const closeHoursLess = 
+          hoursCloseList<0 ?
+          `${(hoursCloseList*(-1))-1} godz.` :
+          ""
+
+          const openMin = 
+          todayMinutes>0 ? 
+          `${60 - todayMinutes} min.` :
+          ""
+        
+          const openHoursMore = 
+          hoursOpenList>0 ? 
+          `${hoursOpenList-1 == 0 ? "" : hoursOpenList-1+" godz."}`  :
+          ""
+
+          const openHoursLess = 
+          hoursOpenList<0 ?
+          `${(hoursOpenList*(-1))-1 == 0 ? "" : hoursOpenList-1+" godz."}`  :
+          ""
+
+          const openHoursMorePrev = 
+          hoursOpenListPrev>0 ? 
+          `${hoursOpenListPrev-1 == 0 ? "" : hoursOpenListPrev-1+" godz."}`  :
+          ""
+
+          const openHoursLessPrev = 
+          hoursOpenListPrev<0 ?
+          `${(hoursOpenListPrev*(-1))-1 == 0 ? "" : hoursOpenListPrev-1+" godz."}`  :
+          ""
+
+          console.log(closeMin)
+
+          console.log(closeHoursMore)
+          console.log(closeHoursLess)
+          
+          return(
         <li key={idx} className={"pure-menu-item"}>
           <span>{item.day} {item.hours}</span>
-          {item.day == plDay ? item.day == plDay && item.open < todayHour  && item.close > todayHour ?  <StillOpen>wciąż otwarte</StillOpen> : <NowClose>już niestety zamknięte</NowClose> : "" }
+          {item.day == plDay ? item.day == plDay && (item.open == todayHour || item.open < todayHour)   && item.close > todayHour ?
+          <StillOpen>zamknięcie za: {openHoursMore ? openHoursMore+""+closeMin : openHoursLess+""+openMin}</StillOpen> : 
+          <NowClose>
+            otwarcie za: {openHoursMorePrev ? openHoursMorePrev+""+closeMin : openHoursLessPrev+""+closeMin}
+          </NowClose> : "" }
+
         </li>
-        )
+        )}
         )
       );
 
--- src/components/OpeningHoursDropDown/openingData.js
+++ src/components/OpeningHoursDropDown/openingData.js
@@ -1,9 +1,9 @@
 export const openingData = [
-    {day: "poniedziałek", hours: "10:00-20:00", open: 10, close: 20},
-    {day: "wtorek", hours: "10:00-20:00", open: 10, close: 20},
-    {day: "środa", hours: "10:00-20:00", open: 10, close: 20},
-    {day: "czwartek", hours: "10:00-20:00", open: 10, close: 20},
-    {day: "piątek", hours: "10:00-20:00", open: 10, close: 20},
-    {day: "sobota", hours: "12:00-18:00", open: 12, close: 18},
-    {day: "niedziela", hours: "12:00-18:00", open: 12, close: 18},
+    {day: "poniedziałek", hours: "10:00-20:00", open: 10, close: 20, prevClose: 10},
+    {day: "wtorek", hours: "10:00-20:00", open: 10, close: 20, prevClose: 10},
+    {day: "środa", hours: "10:00-20:00", open: 10, close: 20, prevClose: 10},
+    {day: "czwartek", hours: "10:00-20:00", open: 10, close: 20, prevClose: 10},
+    {day: "piątek", hours: "10:00-20:00", open: 10, close: 20, prevClose: 12},
+    {day: "sobota", hours: "12:00-18:00", open: 12, close: 18, prevClose: 12},
+    {day: "niedziela", hours: "12:00-18:00", open: 12, close: 18, prevClose: 10},
   ]
